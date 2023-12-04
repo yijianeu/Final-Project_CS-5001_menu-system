011d83a5b972503eb22a7c63bdfe3adb529ff25b
yijianeu <152763320+yijianeu@users.noreply.github.com>
2023-12-03 21:08:44 -0800

    Add files via upload

---
+++ food_data.py
@@ -0,0 +1,6 @@
+food_preferences = {
+    'meat': ['chicken', 'beef', 'pork', 'tofu(vegan)'],
+    'cooking_style': ['grilled', 'stir-fried', 'baked', 'steamed'],
+    'flavor': ['salty', 'sweet', 'spicy', 'sour'],
+    'calories': ['500', '700', '900', '1200']
+}
